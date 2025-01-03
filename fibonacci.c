0707b67aa4bba28be3eab886ddd91cac15149843
harish-chinnasamy11 <harish.chinnasamy@solitontech.com>
2025-01-03 13:03:39 +0530

    adding Fibonacci.c file

---
+++ fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int * generateFibonacciSeries (int size)
+{
+    int * arr = malloc(size * sizeof(int));
+    if (size == 1) {
+        arr[0] = 0;
+    }
+    else if (size == 2) {
+        arr[0] = 0; 
+        arr[1] = 1;
+    }
+    else {
+        arr[0] = 0; 
+        arr[1] = 1;
+        for(int i=2; i<size; i++)
+        {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+    }
+    return arr;
+}
+
+int main() {
+    int size;
+    scanf("%d", &size);
+    
+    int* arr = generateFibonacciSeries(size);
+    for(int i=0; i<size; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+    return 0;
+}
